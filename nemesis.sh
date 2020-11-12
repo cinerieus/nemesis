b7d7ce8a20c4933c8ea5068ee04d86de58cd2c8a
cinereus <cinereus@protonmail.com>
2020-11-12 22:11:28 +0000

    update

--- nemesis.sh
+++ nemesis.sh
@@ -256,6 +256,7 @@ pacman --noconfirm -S base-devel gnu-netcat socat netstat-nat git python python-
 printf "\n\nInstalling Blackarch repos... \n"
 cd /opt
 curl https://blackarch.org/strap.sh -o blackarch.sh
+chmod +x blackarch.sh
 ./blackarch.sh
 pacman --noconfirm -Sy
 
