869c11f084c8cb912cc0d82a4a7b70e41b5d585b
cinereus <cinereus@protonmail.com>
2021-12-16 14:25:28 +0000

    Update nemesis.sh

--- nemesis.sh
+++ nemesis.sh
@@ -297,7 +297,7 @@ yay --noconfirm -Sy
 ## attack build - extra tools ##
 if echo "$extra" | grep -iqF y; then
 	## tools ##
-	pacman --noconfirm -S nmap impacket metasploit sqlmap john medusa ffuf feroxbuster nullinux linux-smart-enumeration enum4linux seclists bloodhound-python ad-ldap-enum ntdsxtract binwalk sleuthkit evil-winrm responder freerdp gowitness acccheck volatility3 cewl strace pspy gittools
+	pacman --noconfirm -S nmap impacket metasploit sqlmap john medusa ffuf feroxbuster nullinux linux-smart-enumeration enum4linux seclists bloodhound-python ad-ldap-enum ntdsxtract binwalk sleuthkit evil-winrm responder freerdp gowitness acccheck volatility3 cewl strace pspy gittools pacu
 	#sudo -Hu $username yay --noconfirm -S libesedb
 	sudo -Hu $username pip install as3nt --no-input --user
 
@@ -362,6 +362,7 @@ if echo "$extra" | grep -iqF y; then
 - strace
 - pspy
 - gittools
+- pacu
 
 ## Scripts ##
 - linux-smart-enumeration
