a11b2b265f74fd4ad73f665443916607505a8b41
cinerieus <cinereus@protonmail.com>
2022-02-13 18:52:47 +0000

    Update nemesis.sh

--- nemesis.sh
+++ nemesis.sh
@@ -298,7 +298,7 @@ pacman --noconfirm -S yay base-devel lib32-glibc gnu-netcat socat python python-
 ## attack build - extra tools ##
 if echo "$extra" | grep -iqF y; then
 	## tools ##
-	pacman --noconfirm -S nmap impacket metasploit sqlmap john medusa ffuf feroxbuster nullinux linux-smart-enumeration enum4linux seclists bloodhound-python ad-ldap-enum ntdsxtract binwalk evil-winrm responder freerdp gowitness miniserve cewl strace pspy gittools scoutsuite pacu subfinder
+	pacman --noconfirm -S nmap impacket metasploit sqlmap john medusa ffuf feroxbuster nullinux linux-smart-enumeration enum4linux seclists bloodhound-python ad-ldap-enum ntdsxtract binwalk evil-winrm responder freerdp gowitness miniserve cewl strace pspy gittools scoutsuite pacu subfinder httpx dnsx
 	sudo -Hu $username pip install as3nt --no-input --user
 
 	## extra ##
@@ -387,6 +387,8 @@ if echo "$extra" | grep -iqF y; then
 - merlin
 - chisel
 - subfinder
+- httpx
+- dnsx
 
 ## Scripts ##
 - linux-smart-enumeration
