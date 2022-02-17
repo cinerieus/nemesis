8da48953db91d0c5c61e04b8c569c8080adb8b8f
cinerieus <cinereus@protonmail.com>
2022-02-17 22:53:19 +0000

    Update nemesis.sh

--- nemesis.sh
+++ nemesis.sh
@@ -436,10 +436,7 @@ if echo "$server" | grep -iqF y; then
 sudo rm /etc/resolv.conf && sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 nvim +:PlugInstall +:qa
 sudo nvim +:PlugInstall +:qa
-yay -S --noconfirm libesedb fish oh-my-fish &&
-git clone https://github.com/cinerieus/theme-sushi.git ~/.local/share/omf/themes/sushi
-wget -q https://raw.githubusercontent.com/cinerieus/nemesis/master/config.fish -O ~/.config/fish/config.fish
-chsh -s /bin/fish
+yay -S --noconfirm libesedb fish
 rm -f finish.sh
 	" > /home/$username/finish.sh
 	
@@ -453,10 +450,7 @@ else
 #\x21/bin/bash
 nvim +:PlugInstall +:qa &&
 sudo nvim +:PlugInstall +:qa &&
-yay -S --noconfirm libesedb fish oh-my-fish &&
-git clone https://github.com/cinerieus/theme-sushi.git ~/.local/share/omf/themes/sushi
-wget -q https://raw.githubusercontent.com/cinerieus/nemesis/master/config.fish -O ~/.config/fish/config.fish
-chsh -s /bin/fish
+yay -S --noconfirm libesedb fish
 rm -f finish.sh
 	" > /home/$username/finish.sh
 fi
