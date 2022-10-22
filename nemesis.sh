0b8774566635e81ca329d35ba3daf83552fbed0e
cinerieus <cinereus@protonmail.com>
2022-10-22 05:02:46 +0100

    Update nemesis.sh

--- nemesis.sh
+++ nemesis.sh
@@ -112,9 +112,9 @@ mount "${diskpart1}" /mnt/boot
 printf "\n\nPackstrap packages...\n"
 # More packages can be added here
 if echo "$server" | grep -iqF y; then
-        pacstrap /mnt base linux lvm2 grub efibootmgr sudo which neovim openssh git
+        pacstrap /mnt base linux lvm2 grub efibootmgr
 else
-	pacstrap /mnt base linux linux-firmware lvm2 grub efibootmgr sudo which neovim openssh git
+	pacstrap /mnt base linux linux-firmware lvm2 grub efibootmgr
 fi
 
 #### Config ####
@@ -205,6 +205,7 @@ if echo "$server" | grep -iqF y; then
                 [DHCP]
                 RouteMetric=20" > /etc/systemd/network/25-wireless.network
         fi
+	rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 	systemctl enable systemd-networkd
 fi
 systemctl enable systemd-resolved
@@ -227,13 +228,29 @@ if echo "$encryption" | grep -iqF y; then
 fi
 grub-mkconfig -o /boot/grub/grub.cfg
 
+#### Pacman Init ####
+printf "\n\nInitializing Pacman... \n"
+curl https://blackarch.org/strap.sh | sh
+echo "Server = http://mirror.zetup.net/blackarch/blackarch/os/x86_64" > /etc/pacman.d/blackarch-mirrorlist
+echo "
+[multilib]
+Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+pacman --noconfirm -Syu 
+pacman --noconfirm -S sudo which neovim openssh git fish toilet lolcat neofetch fortune-mod cowsay
+
 #### User Setup ####
-printf "\n\nUser setup...\n"
+printf "\n\nSetting up low priv user...\n"
 echo "%wheel    ALL=(ALL) ALL" >> /etc/sudoers
 useradd -m -G users,wheel $username
 echo -e "$password\n$password" | passwd $username
+sudo -Hu $username mkdir /home/$username/.ssh
+sudo -Hu $username chmod 750 /home/$username/.ssh
+chgrp users /opt
+chmod 775 /opt
+chmod g+s /opt
 
 #### SSH setup ####
+printf "\n\nConfiguring SSH... \n"
 if echo "$server" | grep -iqF y; then
         systemctl enable sshd
         if [ -n "$sshkeyurl" ]; then
@@ -241,8 +258,11 @@ if echo "$server" | grep -iqF y; then
                 HostKey /etc/ssh/ssh_host_ed25519_key
                 PermitRootLogin no
                 PasswordAuthentication no" >> /etc/ssh/sshd_config
-		sudo -Hu $username mkdir /home/$username/.ssh
-		sudo -Hu $username chmod 750 /home/$username/.ssh
+		echo -e "#\x21/bin/bash" > /etc/motd.sh && \
+                echo "echo \"$(toilet -f pagga -w 110 -F border Nemesis | lolcat -ft)\"" >> /etc/motd.sh && \
+                echo "echo '' ; neofetch ; echo '' ; fortune | cowsay -f head-in -W 110 | lolcat -f ; echo ''" >> /etc/motd.sh && \
+                chmod +x /etc/motd.sh && \
+                echo "session    optional   pam_exec.so          stdout /etc/motd.sh" >> /etc/pam.d/system-login
                 sudo -Hu $username curl $sshkeyurl > /home/$username/.ssh/authorized_keys
 		sudo -Hu $username chmod 600 /home/$username/.ssh/authorized_keys
         else
@@ -253,24 +273,30 @@ if echo "$server" | grep -iqF y; then
 fi
 
 #### Customization ####
-printf "\n\nInstalling packages...\n"
+printf "\n\nCustomizing... \n"
+sudo -u $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/bashrc -o /home/$username/.bashrc
+sudo -u $username curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o /home/$username/.local/share/nvim/site/autoload/plug.vim --create-dirs
+sudo -u $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/init.vim -o /home/$username/.config/nvim/init.vim --create-dirs
+sudo -u $username nvim +:PlugInstall +:qa
+sudo -u $username curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > /home/$username/install.fish
+sudo -u $username fish /home/$username/install.fish --noninteractive && \
+mv /home/$username/install.fish /root
+sudo -u $username git clone https://github.com/cinerieus/theme-sushi.git /home/$username/.local/share/omf/themes/sushi
+sudo -u $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/config.fish -o /home/$username/.config/fish/config.fish
+sudo -u $username fish -c \'omf theme sushi\'
+cp /home/$username/.bashrc /root/.profile
+mkdir -p /root/.local/share/nvim/site/autoload && cp /home/$username/.local/share/nvim/site/autoload/plug.vim /root/.local/share/nvim/site/autoload/plug.vim
+mkdir -p /root/.config/nvim && cp /home/$username/.config/nvim/init.vim /root/.config/nvim/init.vim
+nvim +:PlugInstall +:qa
+fish /root/install.fish --noninteractive
+rm /root/install.fish
+cp -r /home/$username/.local/share/omf/themes/sushi /root/.local/share/omf/themes/
+cp -r /home/$username/.config/fish/config.fish /root/.config/fish/config.fish
+fish -c \'omf theme sushi\'
+usermod -s /bin/fish $username
+usermod -s /bin/fish root
 
-## shared folder ##
-umask 002
-chgrp users /opt
-chmod 775 /opt
-chmod g+s /opt
-cd /opt
-
-## blackarch repos ##
-printf "\n\nInstalling Blackarch repos... \n"
-curl https://blackarch.org/strap.sh | sh
-echo "
-[multilib]
-Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-pacman --noconfirm -Syu
-
-## build specific packages ##
+## build specific setup ##
 if echo "$vm" | grep -iqF y; then
         pacman --noconfirm -S open-vm-tools gtkmm3
 	mkdir -p /etc/xdg/autostart
@@ -288,72 +314,42 @@ fi
 
 ## intel ##
 #pacman --noconfirm -S intel-ucode mesa lib32-mesa vulkan-intel  
-
 ## amd ##
 #pacman --noconfirm -S amd-ucode mesa lib32-mesa amdvlk lib32-amdvlk
-
 ## utils ##
-pacman --noconfirm -S yay base-devel lib32-glibc gnu-netcat socat python python-pip p7zip tmux go cifs-utils wget tcpdump proxychains openvpn wireguard-tools systemd-resolvconf cowsay lolcat fortune-mod neofetch toilet cmatrix asciiquarium
+pacman --noconfirm -S base-devel lib32-glibc wget yay socat python python-pip p7zip tmux go cifs-utils tcpdump proxychains-ng openvpn wireguard-tools systemd-resolvconf cmatrix asciiquarium
 
 ## attack build - extra tools ##
 if echo "$extra" | grep -iqF y; then
 	## tools ##
-	pacman --noconfirm -S nmap impacket metasploit sqlmap john medusa ffuf feroxbuster nullinux linux-smart-enumeration enum4linux seclists bloodhound-python ad-ldap-enum ntdsxtract binwalk evil-winrm responder freerdp gowitness miniserve cewl strace pspy gittools scoutsuite pacu subfinder httpx dnsx gau nuclei interactsh-client
-	sudo -Hu $username pip install as3nt --no-input --user
-
+	pacman --noconfirm -S nmap impacket metasploit sqlmap john medusa ffuf nullinux linux-smart-enumeration seclists bloodhound-python ldapdomaindump ntdsxtract binwalk evil-winrm responder freerdp gowitness miniserve cewl strace pspy gittools scoutsuite pacu subfinder httpx dnsx gau nuclei interactsh-client
 	## extra ##
-	mkdir -p /opt/wordlists /opt/linux /opt/windows /opt/peassng /opt/chisel /opt/c2/merlin /opt/c2/sliver
-	cd /opt/wordlists
-	wget http://downloads.skullsecurity.org/passwords/rockyou.txt.bz2
-	bzip2 -d rockyou.txt.bz2
-	cd /opt
-	
-	## useful binaries ##
-	git clone https://github.com/SecWiki/linux-kernel-exploits.git /opt/linux/linux-kernel-exploits
-	git clone https://github.com/andrew-d/static-binaries.git /opt/linux/static-binaries
-	git clone https://github.com/hugsy/gdb-static.git /opt/linux/gdb-static
-	
-	git clone https://github.com/SecWiki/windows-kernel-exploits.git /opt/windows/windows-kernel-exploits
-	git clone https://github.com/interference-security/kali-windows-binaries.git /opt/windows/binaries
-	git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries.git /opt/windows/ghostpack_binaries
-	git clone https://github.com/dirkjanm/krbrelayx.git /opt/windows/krbrelayx
-	
-	## peass-ng ##
-	wget https://github.com/carlospolop/PEASS-ng/releases/download/20220731/linpeas.sh -O /opt/peassng/linpeas.sh
-	wget https://github.com/carlospolop/PEASS-ng/releases/download/20220731/winPEAS.bat -O /opt/peassng/winPEAS.bat
-	wget https://github.com/carlospolop/PEASS-ng/releases/download/20220731/winPEASx64.exe -O /opt/peassng/winPEASx64.exe
-	wget https://github.com/carlospolop/PEASS-ng/releases/download/20220731/winPEASx86.exe -O /opt/peassng/winPEASx86.exe
-	
-	## merlin ##
-	wget https://github.com/Ne0nd0g/merlin/releases/download/v1.5.0/merlinServer-Linux-x64.7z -O /opt/c2/merlin/merlinServer-Linux-x64.7z
-	7z x /opt/c2/merlin/merlinServer-Linux-x64.7z -o/opt/c2/merlin -pmerlin && rm /opt/c2/merlin/merlinServer-Linux-x64.7z
-	ln -s /opt/c2/merlin/merlinServer-Linux-x64.7z /usr/local/bin/merlin
-	
-	## sliver ##
-	wget https://github.com/BishopFox/sliver/releases/download/v1.5.21/sliver-server_linux -O /opt/c2/sliver/sliver-server
-	wget https://github.com/BishopFox/sliver/releases/download/v1.5.21/sliver-client_linux -O /opt/c2/sliver/sliver-client
-	
-	## chisel ##
-	cd chisel
-	wget https://github.com/jpillora/chisel/releases/download/v1.7.7/chisel_1.7.7_linux_amd64.gz
-	wget https://github.com/jpillora/chisel/releases/download/v1.7.7/chisel_1.7.7_windows_amd64.gz
-	wget https://github.com/jpillora/chisel/releases/download/v1.7.7/chisel_1.7.7_windows_386.gz
-	cd ..
-	
-	## gf ##
-	git clone https://github.com/tomnomnom/gf.git /opt/gf
-	git clone https://github.com/1ndianl33t/Gf-Patterns.git /opt/gf_patterns
-	
-	## 403 bypasses ##
-	git clone https://github.com/iamj0ker/bypass-403.git
-	git clone https://github.com/lobuhi/byp4xx.git
-	
-	## pwnkit (polkit) ##
-	wget https://github.com/ryaagard/CVE-2021-4034/archive/refs/heads/main.zip -O /opt/linux/CVE-2021-4034.zip
+	umask 002
+	mkdir -p /opt/wordlists /opt/linux /opt/windows /opt/peassng /opt/chisel /opt/c2/
+        curl http://downloads.skullsecurity.org/passwords/rockyou.txt.bz2 -o /opt/wordlists/rockyou.bz2
+        curl https://github.com/SecWiki/linux-kernel-exploits/archive/refs/heads/master.zip -o /opt/linux/linux-kernel-exploits.zip
+        curl https://github.com/ryaagard/CVE-2021-4034/archive/refs/heads/main.zip -o /opt/linux/CVE-2021-4034.zip
+        curl https://github.com/andrew-d/static-binaries/archive/refs/heads/master.zip -o /opt/linux/static-binaries.zip
+        curl https://github.com/hugsy/gdb-static/archive/refs/heads/master.zip -o /opt/linux/gdb-static.zip
+        curl https://github.com/SecWiki/windows-kernel-exploits/archive/refs/heads/master.zip -o /opt/windows/windows-kernel-exploits.zip
+        curl https://github.com/interference-security/kali-windows-binaries/archive/refs/heads/master.zip -o /opt/windows/binaries.zip
+        curl https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/archive/refs/heads/master.zip -o /opt/windows/ghostpack_binaries.zip
+        curl https://github.com/dirkjanm/krbrelayx/archive/refs/heads/master.zip -o /opt/windows/krbrelayx.zip
+        curl https://github.com/carlospolop/PEASS-ng/releases/download/20220925/linpeas.sh -o /opt/peassng/linpeas.sh
+        curl https://github.com/carlospolop/PEASS-ng/releases/download/20220925/winPEAS.bat -o /opt/peassng/winPEAS.bat
+        curl https://github.com/carlospolop/PEASS-ng/releases/download/20220925/winPEASx64.exe -o /opt/peassng/winPEASx64.exe
+        curl https://github.com/carlospolop/PEASS-ng/releases/download/20220925/winPEASx86.exe -o /opt/peassng/winPEASx86.exe
+        7z a /opt/peassng/peassng.7z /opt/peassng/* && rm -f /opt/peassng/lin* && rm -f /opt/peassng/win*
+        curl https://github.com/jpillora/chisel/releases/download/v1.7.7/chisel_1.7.7_linux_amd64.gz -o /opt/chisel/chisel_1.7.7_linux_amd64.gz
+        curl https://github.com/jpillora/chisel/releases/download/v1.7.7/chisel_1.7.7_windows_amd64.gz -o /opt/chisel/chisel_1.7.7_windows_amd64.gz
+        curl https://github.com/jpillora/chisel/releases/download/v1.7.7/chisel_1.7.7_windows_386.gz -o /opt/chisel/chisel_1.7.7_windows_386.gz
+        curl https://github.com/Ne0nd0g/merlin/releases/download/v1.5.0/merlinServer-Linux-x64.7z -o /opt/c2/merlin/merlinServer-Linux-x64.7z --create-dirs
+        curl https://github.com/BishopFox/sliver/releases/download/v1.5.28/sliver-server_linux -o /opt/c2/sliver/sliver-server --create-dirs
+        curl https://github.com/BishopFox/sliver/releases/download/v1.5.28/sliver-client_linux -o /opt/c2/sliver/sliver-client
+        7z a /opt/c2/sliver/sliver.7z /opt/c2/sliver/* && rm -f /opt/c2/sliver/sliver-*
 	echo "
 ## Todo ##
 - Change your password from Ch4ngeM3!
-- Run finish.sh
 
 ## Fun ##
 - cowsay
@@ -366,8 +362,8 @@ if echo "$extra" | grep -iqF y; then
 
 ## Tools ##
 - nmap
+- ncat
 - socat
-- netcat
 - openvpn
 - wireguard
 - proxychains
@@ -377,10 +373,9 @@ if echo "$extra" | grep -iqF y; then
 - john-the-ripper
 - medusa
 - ffuf
-- feroxbuster
 - nullinux
-- enum4linux
 - bloodhound-python
+- ldapdomaindump
 - esedbexport
 - ntdsxtract
 - binwalk
@@ -388,6 +383,7 @@ if echo "$extra" | grep -iqF y; then
 - responder
 - freerdp
 - gowitness
+- miniserve
 - cewl
 - strace
 - pspy
@@ -395,16 +391,17 @@ if echo "$extra" | grep -iqF y; then
 - scoutsuite
 - pacu
 - merlin
+- sliver
 - chisel
 - subfinder
 - httpx
 - dnsx
 - gau
-- gf
+- nuclei
+- interactsh-client
 
 ## Scripts ##
 - linux-smart-enumeration
-- ad-ldap-enum
 - PEASS-ng
 - krbrelayx
 
@@ -420,13 +417,11 @@ if echo "$extra" | grep -iqF y; then
 - Tools and scripts are located in /usr/share & /opt
 - SecLists: /usr/share/seclists
 - rockyou.txt: /opt/wordlists/rockyou.txt
-- gf patterns: ~/.gf
 	" > /home/$username/readme.txt
 else
 	echo "
 ## Todo ##
 - Change your password from Ch4ngeM3!
-- Run finish.sh
 
 ## Fun ##
 - cowsay
@@ -439,51 +434,11 @@ else
 	" > /home/$username/readme.txt
 fi
 
-## finishing touches ##
-printf "\n\nFinishing touches... \n"
-
-if echo "$server" | grep -iqF y; then
-	echo -e "
-#\x21/bin/bash
-sudo rm /etc/resolv.conf && sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
-nvim +:PlugInstall +:qa
-sudo nvim +:PlugInstall +:qa
-yay -S --noconfirm libesedb fish
-curl https://raw.githubusercontent.com/cinerieus/nemesis/master/fish.txt -O
-rm -f finish.sh
-	" > /home/$username/finish.sh
-	
-	echo -e "#\x21/bin/bash" > /etc/motd.sh
-	echo "echo \"$(toilet -f pagga -w 110 -F border $hostname | lolcat -ft)\"" >> /etc/motd.sh
-	echo "echo '' ; neofetch ; echo '' ; fortune | cowsay -f head-in -W 110 | lolcat -f ; echo ''" >> /etc/motd.sh
-	chmod +x /etc/motd.sh
-	echo "session    optional   pam_exec.so          stdout /etc/motd.sh" >> /etc/pam.d/system-login
-else
-	echo -e "
-#\x21/bin/bash
-nvim +:PlugInstall +:qa &&
-sudo nvim +:PlugInstall +:qa &&
-yay -S --noconfirm libesedb fish
-curl https://raw.githubusercontent.com/cinerieus/nemesis/master/fish.txt -O
-rm -f finish.sh
-	" > /home/$username/finish.sh
-fi
-
-chmod +x /home/$username/finish.sh
-
 ## secure boot script ##
 if echo "$encryption" | grep -iqF y; then
 	sudo -Hu $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/secure_boot.sh -o /home/$username/secure_boot.sh
 	chmod +x /home/$username/secure_boot.sh
 fi
-
-## nvim config ##
-sudo -Hu $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/bashrc -o /home/$username/.bashrc
-sudo -Hu $username curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o /home/$username/.local/share/nvim/site/autoload/plug.vim --create-dirs
-sudo -Hu $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/init.vim -o /home/$username/.config/nvim/init.vim --create-dirs
-cp /home/$username/.bashrc /root/.profile
-mkdir -p /root/.local/share/nvim/site/autoload && cp /home/$username/.local/share/nvim/site/autoload/plug.vim /root/.local/share/nvim/site/autoload/plug.vim
-mkdir -p /root/.config/nvim && cp /home/$username/.config/nvim/init.vim /root/.config/nvim/init.vim
 
 if echo "$server" | grep -iqFv y; then
 	## font config ##
