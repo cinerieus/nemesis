6f51c9a69d73c527233f1a1f774a2159b93a186b
cinerieus <cinereus@protonmail.com>
2022-12-15 17:25:34 +0000

    Update secure_boot.sh

--- secure_boot.sh
+++ secure_boot.sh
@@ -6,7 +6,7 @@ cd /opt/sb
 
 sudo grub-install --removable --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --sbat=/usr/share/grub/sbat.csv --modules="all_video boot btrfs cat chain configfile echo efifwsetup efinet ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio halt help hfsplus iso9660 jpeg keystatus loadenv loopback linux ls lsefi lsefimmap lsefisystab lssal memdisk minicmd normal ntfs part_apple part_msdos part_gpt password_pbkdf2 png probe reboot regexp search search_fs_uuid search_fs_file search_label sleep smbios test true video xfs zfs zfscrypt zfsinfo play cpuid tpm luks lvm"
 
-yay -S --noconfirm shim-signed sbsigntools
+yay --noconfirm -S shim-signed sbsigntools
 sudo mv /boot/EFI/BOOT/BOOTx64.EFI /boot/EFI/BOOT/grubx64.efi
 sudo cp /usr/share/shim-signed/shimx64.efi /boot/EFI/BOOT/BOOTx64.EFI
 sudo cp /usr/share/shim-signed/mmx64.efi /boot/EFI/BOOT/
