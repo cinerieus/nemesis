0ec456a30507199c14b0d1b050889a14dc4b3997
cinerieus <cinereus@protonmail.com>
2023-02-26 19:50:32 +0000

    Update nemesis.sh

--- nemesis.sh
+++ nemesis.sh
@@ -376,8 +376,12 @@ cp -r /home/$username/.config/fish/config.fish /root/.config/fish/config.fish
 fish -c "omf theme sushi"
 usermod -s /bin/fish $username
 usermod -s /bin/fish root
-sudo -u $username echo "set -g mouse on" > /home/$username/.tmux.conf
-echo "set -g mouse on" > /root/.tmux.conf
+sudo -u git clone https://github.com/tmux-plugins/tpm /home/$username/.tmux/plugins/tpm
+cp -r /home/$username/.tmux/plugins/tpm /root.tmux/plugins/tpm
+sudo -u $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/tmux.conf -o /home/$username/.tmux.conf
+cp /home/$username/.tmux.conf /root/.tmux.conf
+sudo -u $username /home/$username/.tmux/plugins/tpm/scripts/install_plugins.sh
+/root/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 ## build specific setup ##
 if echo "$vm" | grep -iqF y; then
