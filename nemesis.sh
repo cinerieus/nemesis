6b572d67c0da401855edbc38550f9860aa425792
cinerieus <cinereus@protonmail.com>
2023-02-26 20:08:52 +0000

    Update nemesis.sh

--- nemesis.sh
+++ nemesis.sh
@@ -377,6 +377,7 @@ fish -c "omf theme sushi"
 usermod -s /bin/fish $username
 usermod -s /bin/fish root
 sudo -u git clone https://github.com/tmux-plugins/tpm /home/$username/.tmux/plugins/tpm
+mkdir -p /root/.tmux/plugins/tpm
 cp -r /home/$username/.tmux/plugins/tpm /root.tmux/plugins/tpm
 sudo -u $username curl https://raw.githubusercontent.com/cinerieus/nemesis/master/tmux.conf -o /home/$username/.tmux.conf
 cp /home/$username/.tmux.conf /root/.tmux.conf
