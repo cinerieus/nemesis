5411286dbdf8fdc955eed9a928ecd4d7c0575c19
cinerieus <cinereus@protonmail.com>
2023-02-27 10:32:31 +0000

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -23,6 +23,7 @@ kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group o
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///usr/share/wallpapers/dracula/first-collection/arch.png")}'
 
 # Set SDDM theme
+mkdir /etc/sddm.conf.d
 echo "[Autologin]
 Relogin=false
 Session=
